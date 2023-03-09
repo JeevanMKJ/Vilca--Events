d4ef6f3f743696b54e844d0693eeab3e3e856c82
brennaveir <brennaveir@hotmail.com>
2023-03-08 21:02:25 -0500

    added js to handle login an logout, added normalize.css

---
+++ public/js/logOut.js
@@ -0,0 +1,16 @@
+const logout = async () => {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  };
+  
+  document.querySelector('#logout').addEventListener('click', logout);
+  
