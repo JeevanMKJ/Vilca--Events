ab994683337d06ccf0d91d211cd176f48877267b
JeevanMKJ <jeevanjones.jmkj@gmail.com>
2023-03-08 19:05:37 -0500

    set up all the starter code

---
+++ controllers/apiRoutes/handleEventRoutes.js
@@ -0,0 +1,5 @@
+// post to create evnet
+
+// update to update event
+
+// delete to delete event
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,7 @@
+// get to request all events from db
+
+// get redirect to log in/sign up
+
+// get redirect to handle events
+
+// get request event by id
