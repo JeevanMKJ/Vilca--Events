3633b1f21d4c953bef7acceb51a79a3c49dfbe65
Priyanka Agrawal <priyankaagrawal@Priyankas-MacBook-Pro.local>
2023-03-08 21:05:13 -0500

    completed user model and user routes files

--- controllers/apiRoutes/userRoutes.js
+++ controllers/apiRoutes/userRoutes.js
@@ -1,5 +1,67 @@
-// post to sign up
+const router = require('express').Router();
+const { User } = require('../../models');
 
-// post to log in
+// POST Route to signup 
 
-// post to log out
+router.post('/signup', async (req, res) => {
+  try {
+    const selectUser = await User.create(req.body);
+    const newUser = selectUser.get({ plain: true });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      res.status(201).json(newUser);
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'An error has occured' });
+  }
+});
+
+// POST Route to login
+
+router.post('/login', async (req, res) => {
+  try {
+    const selectUser = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!selectUser) {
+      return res
+        .status(404)
+        .json({ message: "This user does not exist! Please create a new account" });
+    }
+
+    const isValidPassword = await selectUser.comparePassword(req.body.password);
+
+    if (!isValidPassword) {
+      return res.status(404).json({ message: 'Wrong password' });
+    }
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      return res
+        .status(200)
+        .json({ message: "You are now logged in!" });
+    });
+  } catch (error) {
+    return res.status(500).json({ message: 'An error has occured' });
+  }
+});
+
+
+// POST route to logout
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+module.exports = router;
+module.exports = User; 
--- models/User.js
+++ models/User.js
@@ -1,1 +1,64 @@
 // id, user_name, user_email, user_password
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    confirmPassword(loginPass) {
+        return bcrypt.compareSync
+        (loginPass, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            },
+        },
+    },
+    {
+        hooks: {
+            beforeCreate: async(newUserInfo) => {
+                newUserInfo.password = await bcrypt.hash(newUserInfo, 10);
+                return newUserInfo
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+module.exports = User;
