b336a0f2efcca265ebab329d12342596e49ed11a
msaylorphila <117216450+msaylorphila@users.noreply.github.com>
2023-03-08 20:06:35 -0500

    finished event model

--- models/Events.js
+++ models/Events.js
@@ -1,3 +1,65 @@
 // id, event_image, event_title, event_date, event_time, event_description, event_location, event_contact_email, event_contact_social, user_id
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
+class Event extends Model {}
 // event_image & event_location refer to MOdule 14, Ex. 14-MVC
+
+Event.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  image: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  time: {
+    type: DataTypes.TIME,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  location: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+  social: {
+    type: DataTypes.STRING,
+    allowNull: yes,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'user',
+      key: 'id',
+    },
+  },
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'user',
+});
+
+module.exports = { Event };
