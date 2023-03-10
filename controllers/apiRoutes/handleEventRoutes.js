70e220633ecb4f50815889ae3ef8e1013be5e74c
Priyanka Agrawal <priyankaagrawal@Priyankas-MacBook-Pro.local>
2023-03-09 21:17:34 -0500

    added handle eventroute for deleting an event

--- controllers/apiRoutes/handleEventRoutes.js
+++ controllers/apiRoutes/handleEventRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const { User } = require('../../models');
-// post to create evnet
+const { Event } = require('../../models');
+// post to create event
 router.post('/add-event', async(req, res) => {
     try {
         const newEvent = await Event.create({
@@ -16,3 +16,25 @@ router.post('/add-event', async(req, res) => {
 // update to update event
 
 // delete to delete event
+router.delete(':/id', async (req, res) => {
+    try {
+        const eventData = await Event.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!eventData) {
+            res.status(404).json({ message: 'No event with this id exists'});
+            return;
+        }
+
+        res.status(200).json(eventData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
+module.exports = Event;
--- controllers/apiRoutes/index.js
+++ controllers/apiRoutes/index.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const handleEventRoutes = require('./handleEventRoutes');
+
 router.use('/users', userRoutes);
+router.use('/events', handleEventRoutes);
+
 module.exports = router;
