761cb03b550f757cebed40f2d3837084a8b37993
Priyanka Agrawal <priyankaagrawal@Priyankas-MacBook-Pro.local>
2023-03-09 21:01:10 -0500

    handle events post route added

--- controllers/apiRoutes/handleEventRoutes.js
+++ controllers/apiRoutes/handleEventRoutes.js
@@ -1,5 +1,18 @@
+const router = require('express').Router();
+const { User } = require('../../models');
 // post to create evnet
+router.post('/add-event', async(req, res) => {
+    try {
+        const newEvent = await Event.create({
+            ...req.body,
+            user_id: req.sessionStore.user_id,
+        });
 
+        res.status(200).json(newEvent);
+    } catch(err) {
+        res.status(400).json(err);
+    }
+});
 // update to update event
 
 // delete to delete event
