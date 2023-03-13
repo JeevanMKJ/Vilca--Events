b78dc3faaf004f1bc364fc4ac4c565b34604b612
msaylorphila <117216450+msaylorphila@users.noreply.github.com>
2023-03-13 19:48:40 -0400

    get my events route for dashboard

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const { Event, User } = require('../models');
+
+// router.get('/', async (req, res) => {
+//   try {
+//     const myEventData = await Event.findAll({
+//       where: {
+//         user_id: req.session.user_id,
+//       },
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['id', 'username'],
+//         },
+//       ],
+//     });
+//     const myEvents = myEventData.map((events) => events.get({ plain: true }));
+//     console.log(myEvents);
+//     const { loggedIn } = req.session;
+//     if (loggedIn) {
+//       res.render('dashboard', {
+//         myEvents,
+//         loggedIn: req.session.loggedIn,
+//       });
+//       return;
+//     }
+//     res.render('login');
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+router.get('/', (req, res) => {
+  res.render('dashboard', { loggedIn: req.session.loggedIn });
+});
+
+module.exports = router;
