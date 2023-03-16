1d7198a897dcba05f5e27b725db9e793f9857a6b
JeevanMKJ <jeevanjones.jmkj@gmail.com>
2023-03-16 17:45:18 -0500

    isUrl finally works

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,14 +1,3 @@
-// module.exports = {
-//   format_date: (date) => {
-//     // Format date as MM/DD/YYYY
-//     return date.toLocaleDateString();
-//   },
-//   // The custom helper 'is_url' takes in a string value and checks if it's a valid URL
-//   isUrl: function (value, options) {
-//     var pattern = /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/;
-//     return pattern.test(value);
-//   },
-// };
 module.exports = {
   format_date: (date) => {
     // Format date as MM/DD/YYYY
@@ -17,17 +6,17 @@ module.exports = {
   isUrl: function (value, options) {
     var pattern = /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/;
     if (pattern.test(value)) {
-      // If the input value is a valid URL, return an anchor tag containing the URL
-      // return `<a href="${value}">${value}</a>`;
       console.log("I AM HERE", value);
-      return "<a>" + options.fn({ test: value }) + "</a>";
+      return (
+        "<a href='" +
+        options.fn({ test: value }) +
+        "'>" +
+        options.fn({ test: value }) +
+        "</a>"
+      );
     } else {
-      // If the input value is not a valid URL, return a paragraph tag containing the value
-      // return `<p>${value}</p>`;
       console.log("I AM HERE!!!!!!!!!!!!", value);
-      return "<a>" + options.fn({ test: value }) + "</a>";
+      return "<p>" + options.fn({ test: value }) + "</p>";
     }
   },
 };
-
-// `<a href="${value}">${value}</a>`
