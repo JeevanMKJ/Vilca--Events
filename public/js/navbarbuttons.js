b3fb5dbfbc626ac56c97627bbd87311f643b2dea
msaylorphila <117216450+msaylorphila@users.noreply.github.com>
2023-03-19 15:48:40 -0400

    homepage button fixed

--- public/js/navbarbuttons.js
+++ public/js/navbarbuttons.js
@@ -18,10 +18,20 @@ const getLogin = async (event) => {
   document.location.replace('/login');
 };
 
+const getHomepage = async (event) => {
+  event.preventDefault();
+  document.location.replace('/');
+};
+
 document.querySelector('#dashboard').addEventListener('click', getDashboard);
 
 document.querySelector('#create').addEventListener('click', getCreate);
 
 document.querySelector('#saved').addEventListener('click', getSaved);
 
+document.querySelector('#homepagelink').addEventListener('click', getHomepage);
+
 document.querySelector('#login').addEventListener('click', getLogin);
+
+
+
